e9507669e52e0fb01ba591f0eef246324fedf282
Ritika Banerjee <109010965+ritika-banerjee@users.noreply.github.com>
2026-07-10 08:36:13 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
